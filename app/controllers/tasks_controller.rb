cb757c996f9831ba9b9b236e25856efa36714db3
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-02-23 17:15:42 +0300

    delete function to be built

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,44 @@
+class TasksController < ApplicationController
+
+  #Read
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # Create
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task)
+  end
+
+  # Update
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
---
+++ db/migrate/20210223094043_change_completedin_tasks.rb
@@ -0,0 +1,5 @@
+class ChangeCompletedinTasks < ActiveRecord::Migration[6.0]
+  def change
+    change_column_default :tasks, :completed, false
+  end
+end
